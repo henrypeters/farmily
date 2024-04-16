8da7e88f80bc91c36af89ab0e612fef416196649
henrypeters <meethenry09@gmail.com>
2024-04-16 21:18:46 +0100

    feat: add arrows for slide image

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+let slideIndex = 1
+showSlides(slideIndex)
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length){
+        slideIndex = 1;
+    }
+
+    if (n < 1){
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+}
 
 
 
