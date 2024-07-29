932226cd3fce54a4fc03be9e6fc20355291c1b8f
henrypeters <meethenry09@gmail.com>
2024-07-29 12:31:03 +0100

    feat: adding livestock products to cart

--- shoppingCart/cart/index.js
+++ shoppingCart/cart/index.js
@@ -1,67 +1,4 @@
-// const firstName = document.getElementById("name1");
-// const secondName = document.getElementById("name2");
-// const thriddName = document.getElementById("name3");
-// const fourthName = document.getElementById("name4");
-// const fifthName = document.getElementById("name5");
-// const sixthName = document.getElementById("name6");
-// const seventhName = document.getElementById("name7");
-// const eighthName = document.getElementById("name8");
-
-// const item1 = document.getElementById("firstItem")
-// const item2 = document.getElementById("secondItem");
-// const item3 = document.getElementById("thirdItem")
-// const item4 = document.getElementById("fourthItem")
-// const item5 = document.getElementById("fifthItem")
-// const item6 = document.getElementById("sixthItem")
-// const item7 = document.getElementById("sevenItem")
-// const item8 = document.getElementById("eightItem")
-
-// const itemContainer = document.getElementById("itemHold")
-// const price1 = document.getElementById("firstPrice")
-// const price2 = document.getElementById("secondPrice")
-// const price3 = document.getElementById("thirdPrice")
-// const price4 = document.getElementById("fourthPrice")
-// const price5 = document.getElementById("fivePrice")
-// const price6 = document.getElementById("sixPrice")
-// const price7 = document.getElementById("sevenPrice")
-// const price8 = document.getElementById("eightPrice")
-
-
-// firstName.innerHTML = localStorage.getItem("pro1");
-// secondName.innerHTML = localStorage.getItem("pro2");
-// const numItem1 = document.getElementById("noItem");
-
-// const rm1 = document.getElementById("rmBtn1");
-// const rm2 = document.getElementById("rmBtn2");
-// const rm3 = document.getElementById("rmBtn3");
-// const rm4 = document.getElementById("rmBtn4");
-// const rm5 = document.getElementById("rmBtn5");
-// const rm6 = document.getElementById("rmBtn6");
-// const rm7 = document.getElementById("rmBtn7");
-// const rm8 = document.getElementById("rmBtn8");
-
-// rm1.addEventListener("click", function(){
-//     localStorage.removeItem("pro1");
-//     item1.style.display = "none"
-// })
-
-// price1.innerHTML = `₦` + localStorage.getItem("price1");
-// numItem1.innerHTML = localStorage.getItem("price1No") + ` item`
-
-// if (firstName.innerHTML === ""){
-//     item1.style.display = "none";
-// }  
-
-// if (secondName.innerHTML === ""){
-//     item2.style.display = "none";
-// }
-
-// if (firstName.innerHTML ==- "" && secondName.innerHTML === ""){
-//     itemContainer.innerHTML = `<div class="notification"><p>Your Cart Is Empty</p></div>`
-// }
-
 const itemContainer = document.getElementById("itemHold")
-
 
 const firstName = document.getElementById("name1");
 const secondName = document.getElementById("name2");
@@ -80,6 +17,39 @@ const fourteenName = document.getElementById("name14");
 const fifteenName = document.getElementById("name15");
 const sixteenName = document.getElementById("name16");
 const seventeenName = document.getElementById("name17");
+const eighteenName = document.getElementById("name18");
+const nineteenName = document.getElementById("name19");
+const twenName = document.getElementById("name20");
+const twenOneName = document.getElementById("name21");
+const twenTwoName = document.getElementById("name22");
+// const twenThreeName = document.getElementById("name23")
+// const twenFourName = document.getElementById("name24")
+// const twenFiveName = document.getElementById("name25")
+// const twenSixName = document.getElementById("name26")
+// const twenSevenName = document.getElementById("name27")
+// const twenEightName = document.getElementById("name28")
+// const twenNineName = document.getElementById("name29")
+// const thirtName = document.getElementById("name30")
+// const thirtOneName = document.getElementById("name31")
+// const thirtTwoName = document.getElementById("name32")
+// const thirtThreeName = document.getElementById("name33")
+// const thirtFourName = document.getElementById("name34")
+// const thirtFiveName = document.getElementById("name35")
+// const thirtSixName = document.getElementById("name36")
+// const thirtSevenName = document.getElementById("name37")
+// const thirtEightName = document.getElementById("name38")
+// const thirtNineName = document.getElementById("name39")
+// const FortName = document.getElementById("name40")
+// const FortOneName = document.getElementById("name41")
+// const FortTwoName = document.getElementById("name42")
+// const FortThreeName = document.getElementById("name43")
+// const FortFourName = document.getElementById("name44")
+// const FortFiveName = document.getElementById("name45")
+// const FortSixName = document.getElementById("name46")
+// const FortSevenName = document.getElementById("name47")
+// const FortEightName = document.getElementById("name48")
+// const FortNineName = document.getElementById("name49")
+// const FiftyName = document.getElementById("name50")
 
 const numItem1 = document.getElementById("numOfitem1");
 const numItem2 = document.getElementById("numOfitem2");
@@ -98,6 +68,23 @@ const numItem14 = document.getElementById("numOfitem14");
 const numItem15 = document.getElementById("numOfitem15");
 const numItem16 = document.getElementById("numOfitem16");
 const numItem17 = document.getElementById("numOfitem17");
+const numItem18 = document.getElementById("numOfitem18");
+const numItem19 = document.getElementById("numOfitem19");
+const numItem20 = document.getElementById("numOfitem20");
+const numItem21 = document.getElementById("numOfitem21");
+const numItem22 = document.getElementById("numOfitem22");
+// const numItem23 = document.getElementById("numOfitem23");
+// const numItem24 = document.getElementById("numOfitem24");
+// const numItem25 = document.getElementById("numOfitem25");
+// const numItem26 = document.getElementById("numOfitem26");
+// const numItem27 = document.getElementById("numOfitem27");
+// const numItem28 = document.getElementById("numOfitem28");
+// const numItem29 = document.getElementById("numOfitem29");
+// const numItem30 = document.getElementById("numOfitem30");
+// const numItem31 = document.getElementById("numOfitem31");
+// const numItem32 = document.getElementById("numOfitem32");
+// const numItem33 = document.getElementById("numOfitem33");
+// const numItem34 = document.getElementById("numOfitem34");
 
 const price1 = document.getElementById("firstPrice")
 const price2 = document.getElementById("secondPrice")
@@ -116,6 +103,11 @@ const price14 = document.getElementById("fourteenPrice")
 const price15 = document.getElementById("fifteenPrice")
 const price16 = document.getElementById("sixteenPrice")
 const price17 = document.getElementById("seventeenPrice")
+const price18 = document.getElementById("eighteenPrice")
+const price19 = document.getElementById("nineteenPrice")
+const price20 = document.getElementById("twenPrice")
+const price21 = document.getElementById("twenOnePrice")
+const price22 = document.getElementById("twenTwoPrice")
 
 const rm1 = document.getElementById("rmBtn1");
 const rm2 = document.getElementById("rmBtn2");
@@ -134,6 +126,25 @@ const rm14 = document.getElementById("rmBtn14");
 const rm15 = document.getElementById("rmBtn15");
 const rm16 = document.getElementById("rmBtn16");
 const rm17 = document.getElementById("rmBtn17");
+const rm18 = document.getElementById("rmBtn18");
+const rm19 = document.getElementById("rmBtn19");
+const rm20 = document.getElementById("rmBtn20");
+const rm21 = document.getElementById("rmBtn21");
+const rm22 = document.getElementById("rmBtn22");
+// const rm23 = document.getElementById("rmBtn23");
+// const rm24 = document.getElementById("rmBtn24");
+// const rm25 = document.getElementById("rmBtn25");
+// const rm26 = document.getElementById("rmBtn26");
+// const rm27 = document.getElementById("rmBtn27");
+// const rm28 = document.getElementById("rmBtn28");
+// const rm29 = document.getElementById("rmBtn29");
+// const rm30 = document.getElementById("rmBtn30");
+// const rm31 = document.getElementById("rmBtn31");
+// const rm32 = document.getElementById("rmBtn32");
+// const rm33 = document.getElementById("rmBtn33");
+// const rm34 = document.getElementById("rmBtn34");
+// const rm35 = document.getElementById("rmBtn35");
+// const rm36 = document.getElementById("rmBtn36");
 
 const item1 = document.getElementById("firstItem");
 const item2 = document.getElementById("secondItem");
@@ -152,6 +163,11 @@ const item14 = document.getElementById("fourteenItem");
 const item15 = document.getElementById("fifteenItem");
 const item16 = document.getElementById("sixteenItem");
 const item17 = document.getElementById("seventeenItem");
+const item18 = document.getElementById("eighteenItem");
+const item19 = document.getElementById("nineteenItem");
+const item20 = document.getElementById("twenItem");
+const item21 = document.getElementById("twenOneItem");
+const item22 = document.getElementById("twenTwoItem");
 
 
 
@@ -172,6 +188,12 @@ fourteenName.value = localStorage.getItem("straw");
 fifteenName.value = localStorage.getItem("mixedFeed");
 sixteenName.value = localStorage.getItem("silage");
 seventeenName.value = localStorage.getItem("sorghumGrain");
+eighteenName.value = localStorage.getItem("cattle")
+nineteenName.value = localStorage.getItem("pig")
+twenName.value = localStorage.getItem("goat")
+twenOneName.value = localStorage.getItem("ram")
+twenTwoName.value = localStorage.getItem("chicken")
+
 
 if (firstName.value === ""){
     item1.style.display = "none";
@@ -242,12 +264,34 @@ if (seventeenName.value === ""){
     item17.style.display = "none";
 }
 
+if (eighteenName.value === ""){
+    item18.style.display = "none";
+}
+
+if (nineteenName.value === ""){
+    item19.style.display = "none";
+}
+
+if (twenName.value === ""){
+    item20.style.display = "none";
+}
+
+if (twenOneName.value === ""){
+    item21.style.display = "none";
+}
+
+if (twenTwoName.value === ""){
+    item22.style.display = "none";
+}
+
 if (firstName.value ==- "" && secondName.value === "" && thriddName.value === "" 
 && fourthName.value === ""  && fifthName.value === ""  && sixthName.value === ""
 && seventhName.value === "" && eighthName.value === "" && ninthName.value === ""
 && tenthName.value === "" && eleventhName.value === "" && twelvethName.value === ""
 && thirteenName.value === "" && fourteenName.value === "" && fifteenName.value === ""
-&& sixteenName.value === "" && seventeenName.value === ""){
+&& sixteenName.value === "" && seventeenName.value === "" && eighteenName.value === ""
+&& nineteenName.value === "" && twenName.value === "" && twenOneName.value === ""
+&& twenTwoName.value === ""){
     itemContainer.innerHTML = `<div class="notification">
                                 <img src="../../images/icons/big-emptyIcon.png" alt="">
                                 <p>Your Cart Is Empty</p>
@@ -350,7 +394,7 @@ numItem8.innerHTML = localStorage.getItem("milkNum")  + ` item`;
 numItem9.innerHTML = localStorage.getItem("pastureNum")  + ` item`;
 numItem10.innerHTML = localStorage.getItem("barleyNum")  + ` item`;
 numItem11.innerHTML = localStorage.getItem("cornNum")  + ` item`;
-numItem12.innerHTML = localStorage.getItem("dryRoughageNum")  + ` item`;
+numItem12.innerHTML = localStorage.getItem("dryRoughageNum")  + ` item`; 
 numItem13.innerHTML = localStorage.getItem("oatNum")  + ` item`;
 numItem14.innerHTML = localStorage.getItem("strawNum")  + ` item`;
 numItem15.innerHTML = localStorage.getItem("mixedFeedNum")  + ` item`;
